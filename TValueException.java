fe1f8a4f607a964f982fc10b9acd6951c28e595c
VirusedParadox <jimdedrummer99@gmail.com>
2017-10-19 20:23:00 +0200

    Add files via upload

---
+++ TValueException.java
@@ -0,0 +1,17 @@
+package org.virusparadox.exceptions;
+
+public class TValueException extends Exception {
+	
+	
+	
+	public TValueException()
+	{
+		
+	}
+	
+	public TValueException(String message)
+	{
+		super(message);
+	}
+
+}
